8c091ad318177cce611ae3fa2aab890e9b841207
Ulises Rosas <ulisesfrosasp@gmail.com>
2024-11-15 17:32:19 -0600

    branch lengths transformed

--- sims/prota_sims/h2_branch_length_optimization.sh
+++ sims/prota_sims/h2_branch_length_optimization.sh
@@ -1,11 +1,11 @@
-aln='/Users/ulises/Desktop/GOL/data/alldatasets/nt_aln/internally_trimmed/malns_36_mseqs_27/round2/no_lavaretus/McL/mysupermatrix.txt'
-partitions='/Users/ulises/Desktop/GOL/data/alldatasets/nt_aln/internally_trimmed/malns_36_mseqs_27/round2/no_lavaretus/McL/mypartitions.txt'
-twohypos='/Users/ulises/Desktop/ABL/software/dissectingFactors/sims/prota_sims/prota_myboth_hypos.txt'
+aln='/Users/ulisesrosas/Desktop/dissectingFactors/sims/prota_sims/mysupermatrix.txt'
+partitions='/Users/ulisesrosas/Desktop/dissectingFactors/sims/prota_sims/mypartitions.txt'
+twohypos='/Users/ulisesrosas/Desktop/dissectingFactors/sims/prota_sims/prota_myboth_hypos.txt'
+iqtree2='/Users/ulisesrosas/Desktop/iqtree-2.3.6-macOS/bin/iqtree2'
+
 
 sed -n 2p $twohypos > tree_h2.txt
 
-
 # error at 9558
-iqtree2 -s $aln -p $partitions -g tree_h2.txt --prefix h2_constrained -T 15
-
+$iqtree2 -s $aln -p $partitions -g tree_h2.txt --prefix h2_constrained -T 15
 
